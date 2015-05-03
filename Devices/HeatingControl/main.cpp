802930759eefffce5bac881f79d28d7912dbb3d4
goulou <fredericg_99@yahoo.fr>
2015-05-03 17:10:32 +0200

    update HeatingControl

--- Devices/HeatingControl/main.cpp
+++ Devices/HeatingControl/main.cpp
@@ -1,13 +1,15 @@
 // Example sketch showing how to send in OneWire temperature readings
 #include <MySensor.h>
 #include <Arduino.h>
+#include <HardwareSerial.h>
 #include <EEPROM.h>
 #include <LiquidCrystal.h>
 
-#define RELAY_1  22  // Arduino Digital I/O pin number for first relay (second on pin+1 etc)
-#define NUMBER_OF_RELAYS 1 // Total number of attached relays
-#define RELAY_ON 1  // GPIO value to write to turn on attached relay
-#define RELAY_OFF 0 // GPIO value to write to turn off attached relay
+#define RELAY_1_ID   16  // MySensors node ID of relay 1
+#define RELAY_1_PIN  22  // Arduino Digital I/O pin number for first relay (second on pin+1 etc)
+#define NUMBER_OF_RELAYS 16 // Total number of attached relays
+#define RELAY_ON 0  // GPIO value to write to turn on attached relay
+#define RELAY_OFF 1 // GPIO value to write to turn off attached relay
 
 MySensor gw(/*_cepin=*/14, /*_cspin=*/15);
 boolean receivedConfig = false;
@@ -24,6 +26,9 @@ void incomingMessage(const MyMessage &message);
 
 void setup()
 {
+	Serial.begin(115200);
+	Serial.println("Launched");
+
 	lcd.begin(16, 2);
 	lcd.clear();
 	lcd.setCursor(0, 0);
@@ -32,8 +37,6 @@ void setup()
 	lcd.print(__TIME__);
 	delay(1000);
 
-	Serial.begin(115200);
-	Serial.println("Launched");
 
 //	for (int i = 0; i < 512; i++)
 //	{
@@ -46,7 +49,7 @@ void setup()
 //	delay(1000);
 	// Initialize library and add callback for incoming messages
 
-	gw.begin(incomingMessage, AUTO, false, AUTO, RF24_PA_MAX);
+	gw.begin(incomingMessage, AUTO, true, AUTO, RF24_PA_MAX);
 	// Send the sketch version information to the gateway and Controller
 	gw.sendSketchInfo("Relay", "1.0");
 
@@ -59,8 +62,10 @@ void setup()
 	lcd.print(gw.getNodeId());
 
 	// Fetch relay status
-	for (int sensor = 1, pin = RELAY_1; sensor <= NUMBER_OF_RELAYS; sensor++, pin++)
+	for (int i=0; i < NUMBER_OF_RELAYS; i++)
 	{
+		int sensor = RELAY_1_ID + i;
+		int pin = RELAY_1_PIN + i;
 		Serial.print("Presenting relay ");
 		Serial.println(sensor);
 		// Register all sensors to gw (they will be created as child devices)
@@ -87,17 +92,17 @@ void incomingMessage(const MyMessage &message)
 	if (message.type == V_LIGHT)
 	{
 		// Change relay state
-		digitalWrite(message.sensor - 1 + RELAY_1, message.getBool() ? RELAY_ON : RELAY_OFF);
+		digitalWrite((message.sensor-RELAY_1_ID) + (RELAY_1_PIN), message.getBool() ? RELAY_ON : RELAY_OFF);
 		// Store state in eeprom
 		gw.saveState(message.sensor, message.getBool());
 		// Write some debug info
-		Serial.print("Incoming change for sensor:");
+		Serial.print("Incoming change for Relay:");
 		Serial.print(message.sensor);
 		Serial.print(", New status: ");
 		Serial.println(message.getBool());
 		lcd.clear();
 		lcd.setCursor(0, 0);
-		lcd.print("Sensor changed");
+		lcd.print("Relay changed");
 		lcd.setCursor(0, 1);
 		lcd.print(message.sensor);
 		lcd.print(" ");
