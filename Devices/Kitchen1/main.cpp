d028906addac5e62d21eb8730aa2cb17aaead36f
goulou <fredericg_99@yahoo.fr>
2015-10-07 21:32:12 +0200

    do not set PA level in kitchen

--- Devices/Kitchen1/main.cpp
+++ Devices/Kitchen1/main.cpp
@@ -23,7 +23,7 @@ void setup()
 
 	// Startup and initialize MySensors library. Set callback for incoming messages.
 	gw.begin(NULL, 0xA2, true, GATEWAY_ADDRESS);
-	gw.setPALevel(RF24_PA_MAX);
+//	gw.setPALevel(RF24_PA_MAX);
 
 	// Send the sketch version information to the gateway and Controller
 	gw.sendSketchInfo(xstr(PROGRAM_NAME), "1.0");
