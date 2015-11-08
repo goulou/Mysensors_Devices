9685c332595c188f0df6a013d31a341592cd2467
goulou <fredericg_99@yahoo.fr>
2015-11-08 15:04:23 +0100

    update eeprom_reset (check pin state for 200ms minimum)

--- Common/eeprom_reset.cpp
+++ Common/eeprom_reset.cpp
@@ -14,19 +14,34 @@
 
 void eeprom_reset_check(uint8_t reset_pin)
 {
+	digitalWrite(reset_pin, LOW);
 	pinMode(reset_pin, INPUT);
 	unsigned long end = millis()+1000;
 	while(millis() < end)
 	{
 		if(digitalRead(reset_pin) == HIGH)
 		{
-			DEBUG_PRINT_ln("RESET PIN HIGH : clearing EEPROM");
-			for (int i=0;i<EEPROM.length();i++)
+			unsigned long end2 = millis()+200;
+			bool ok = true;
+			while(millis() < end2)
 			{
-				EEPROM.write(i, 0xff);
+				if(digitalRead(reset_pin) == LOW)
+				{
+					break;
+					ok = false;
+				}
+			}
+			if(ok)
+			{
+				DEBUG_PRINT_ln("RESET PIN HIGH : clearing EEPROM");
+				for (int i=0;i<EEPROM.length();i++)
+				{
+					EEPROM.write(i, 0xff);
+				}
 			}
 			break;
 		}
 	}
+
 	DEBUG_PRINT_ln("Not clearing EEPROM");
 }
