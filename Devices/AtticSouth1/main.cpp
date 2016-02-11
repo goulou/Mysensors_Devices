e72034946cf7942560795c32c19270180eca8e44
goulou <fredericg_99@yahoo.fr>
2016-02-11 19:38:24 +0100

    add usage of relay_node

--- Devices/AtticSouth1/main.cpp
+++ Devices/AtticSouth1/main.cpp
@@ -3,13 +3,13 @@
 #include <MySensor.h>
 #include <Arduino.h>
 #include <HardwareSerial.h>
-#include <EEPROM.h>
 #include <eeprom_reset.hpp>
 #include <LiquidCrystal.h>
 
 #include <dht_node.hpp>
 #include <1w_node.hpp>
 #include <printf.hpp>
+#include <relay_node.hpp>
 
 MyTransportNRF24 transport(/*_cepin=*/14, /*_cspin=*/15, RF24_PA_MAX);
 MyHwDriver hw;
@@ -35,25 +35,12 @@ unsigned long SLEEP_TIME = 30000; // Sleep time between reads (in milliseconds)
 
 
 #define NUMBER_OF_RELAYS 16 // Total number of attached relays
-const uint8_t relay_pins[NUMBER_OF_RELAYS] PROGMEM = {22, 23, 24, 25, 26, 27, 28, 29, 34, 35, 36, 37, 38, 39, 40, 41};
-const uint8_t relay_ids [NUMBER_OF_RELAYS] PROGMEM = {16, 17, 18, 19, 20, 21, 22, 23, 32, 33, 34, 35, 36, 37, 38, 39};
-#define RELAY_ON 0  // GPIO value to write to turn on attached relay
-#define RELAY_OFF 1 // GPIO value to write to turn off attached relay
+const uint8_t relay_pins[NUMBER_OF_RELAYS] PROGMEM = {22, 24, 26, 28, 23, 25, 27, 29, 30, 32, 34, 36, 31, 33, 35, 37};
+const uint8_t relay_ids [NUMBER_OF_RELAYS] PROGMEM = {22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37};
+#define RELAY_ON LOW  // GPIO value to write to turn on attached relay
+#define RELAY_OFF HIGH // GPIO value to write to turn off attached relay
 
 void incomingMessage(const MyMessage &message);
-
-uint8_t get_relay_pin_for_id(uint8_t sensor_id)
-{
-	for(int i=0; i<NUMBER_OF_RELAYS; i++)
-	{
-		if(pgm_read_byte(&relay_ids[i]) == sensor_id)
-		{
-			return pgm_read_byte(&relay_pins[i]);
-		}
-	}
-	return 255;
-}
-
 
 void setup()
 {
@@ -62,19 +49,11 @@ void setup()
 
 	printf_begin();
 
-	//IO PIN 3
-	eeprom_reset_check(5);
+	//IO PIN 5
+	eeprom_reset_check(3);
 
-	for (int i=0; i < NUMBER_OF_RELAYS; i++)
-	{
-		int sensor_id = pgm_read_byte(&relay_ids[i]);
-		int pin = get_relay_pin_for_id(sensor_id);
-		Serial.print("Having relay ");
-		Serial.print(sensor_id);
-		Serial.print(" at pin ");
-		Serial.println(pin);
-	}
 
+	setup_relay(gw, relay_pins, relay_ids, NUMBER_OF_RELAYS, RELAY_ON, RELAY_OFF, false);
 	setup_dht(gw, HUMIDITY_SENSOR_DIGITAL_PIN, 5, false);
 
 	// Startup and initialize MySensors library. Set callback for incoming messages.
@@ -88,28 +67,7 @@ void setup()
 	present_dht(gw);
 
 	DEBUG_PRINT_ln("Registering relays");
-	// Fetch relay status
-	for (int i=0; i < NUMBER_OF_RELAYS; i++)
-	{
-		int sensor = pgm_read_byte(&relay_ids[i]);
-		int pin = get_relay_pin_for_id(sensor);
-		Serial.print("Presenting relay ");
-		Serial.print(sensor);
-		Serial.print(" at pin ");
-		Serial.println(pin);
-		// Register all sensors to gw (they will be created as child devices)
-		gw.present(sensor, V_LIGHT);
-		// Then set relay pins in output mode
-		pinMode(pin, OUTPUT);
-		// Set relay to last known state (using eeprom storage)
-		digitalWrite(pin, gw.loadState(sensor) ? RELAY_ON : RELAY_OFF);
-		// Change to V_LIGHT if you use S_LIGHT in presentation below
-		MyMessage msg(sensor, V_TRIPPED);
-		bool value = gw.loadState(sensor);
-		gw.send(msg.set(value == HIGH ? 1 : 0));
-	}
-
-
+	present_relays(gw);
 }
 
 void loop()
@@ -117,16 +75,12 @@ void loop()
 	// Process incoming messages (like config from server)
 	gw.process();
 	onewire_node_loop(gw);
-
-	unsigned long sleep_time = SLEEP_TIME;
-	if(loop_dht(gw) == false)
-	{
-		sleep_time = SLEEP_TIME/10;
-	}
+	loop_relays(gw);
+	loop_dht(gw);
 
 	//Repeater mode needs not sleeping : process messages instead.
 	unsigned long start_millis = millis();
-	while( ((unsigned long)(millis() - start_millis)) < sleep_time)
+	while( ((unsigned long)(millis() - start_millis)) < SLEEP_TIME)
 	{
 		gw.process();
 	}
@@ -136,18 +90,9 @@ void loop()
 void incomingMessage(const MyMessage &message)
 {
 	// We only expect one type of message from controller. But we better check anyway.
-	if (message.type == V_LIGHT)
+	if (incoming_message_relay(gw, message))
 	{
-		// Change relay state
-		int pin = get_relay_pin_for_id(message.sensor);
-		digitalWrite(pin, message.getBool() ? RELAY_ON : RELAY_OFF);
-		// Store state in eeprom
-		gw.saveState(message.sensor, message.getBool());
-		// Write some debug info
-		Serial.print("Incoming change for relay:");
-		Serial.print(message.sensor);
-		Serial.print(", New status: ");
-		Serial.println(message.getBool());
+		Serial.println("Incomming msg was for relay");
 	}
 	else
 	{
--- Devices/Basement1/main.cpp
+++ Devices/Basement1/main.cpp
@@ -1,74 +1,62 @@
-// Example sketch showing how to send in OneWire temperature readings
 #include <MySensor.h>
 #include <SPI.h>
 #include <DallasTemperature.h>
 #include <OneWire.h>
 #include <DHT.h>
 
-unsigned long SLEEP_TIME = 30000; // Sleep time between reads (in milliseconds)
-
+#include <eeprom_reset.hpp>
 #include <printf.hpp>
 #include <dht_node.hpp>
 #include <1w_node.hpp>
+#include <relay_node.hpp>
 
+#include <si7021_node.hpp>
 
-#define HUMIDITY_SENSOR_DIGITAL_PIN 4
+unsigned long SLEEP_TIME = 30000; // Sleep time between reads (in milliseconds)
+
+#define HUMIDITY_SENSOR_DIGITAL_PIN A1
+
+MySensor gw;
 
 
 /**
  * My Sensors
  */
-MySensor gw;
-boolean receivedConfig = false;
-void incomingMessage(const MyMessage &message);
-
-
-
-#define RELAY_1_ID   16  // MySensors node ID of relay 1
-#define RELAY_1_PIN  5  // Arduino Digital I/O pin number for first relay (second on pin+1 etc)
 #define NUMBER_OF_RELAYS 1 // Total number of attached relays
-#define RELAY_ON 1  // GPIO value to write to turn on attached relay
-#define RELAY_OFF 0 // GPIO value to write to turn off attached relay
+const uint8_t relay_pins[NUMBER_OF_RELAYS] PROGMEM = {6};
+const uint8_t relay_ids [NUMBER_OF_RELAYS] PROGMEM = {16};
+#define RELAY_ON 0  // GPIO value to write to turn on attached relay
+#define RELAY_OFF 1 // GPIO value to write to turn off attached relay
 
-
+void incomingMessage(const MyMessage &message);
 
 void setup()
 {
+	Serial.begin(BAUD_RATE);
+	Serial.println("launched");
 	printf_begin();
 
+	//IO PIN 5
+	eeprom_reset_check(A3);
 
+
+	setup_relay(gw, relay_pins, relay_ids, NUMBER_OF_RELAYS, RELAY_ON, RELAY_OFF, false);
+	analogWrite(A1, 0);
+	pinMode(A1, OUTPUT);
+	setup_dht(gw, 8, 5, false);
 
 	// Startup and initialize MySensors library. Set callback for incoming messages.
-	gw.begin(incomingMessage, AUTO, true);
+	gw.begin(incomingMessage, 0x1B, true);
 
 	// Send the sketch version information to the gateway and Controller
 	gw.sendSketchInfo(xstr(PROGRAM_NAME), "1.0");
 
-	setup_dht(gw, HUMIDITY_SENSOR_DIGITAL_PIN, 0);
 	onewire_node_setup(gw);
 
-	DEBUG_PRINT_ln("Registering relay");
-	// Fetch relay status
-	for (int i=0; i < NUMBER_OF_RELAYS; i++)
-	{
-		int sensor = RELAY_1_ID + i;
-		int pin = RELAY_1_PIN + i;
-		Serial.print("Presenting relay ");
-		Serial.print(sensor);
-		Serial.print(" at pin ");
-		Serial.println(pin);
-		// Register all sensors to gw (they will be created as child devices)
-		gw.present(sensor, V_LIGHT);
-		// Then set relay pins in output mode
-		pinMode(pin, OUTPUT);
-		// Set relay to last known state (using eeprom storage)
-		digitalWrite(pin, gw.loadState(sensor) ? RELAY_ON : RELAY_OFF);
-		// Change to V_LIGHT if you use S_LIGHT in presentation below
-		MyMessage msg(sensor, V_TRIPPED);
-		bool value = gw.loadState(sensor);
-		gw.send(msg.set(value == HIGH ? 1 : 0));
-	}
+	present_dht(gw);
 
+	DEBUG_PRINT_ln("Registering relays");
+	present_relays(gw);
 
 }
 
@@ -77,16 +65,12 @@ void loop()
 	// Process incoming messages (like config from server)
 	gw.process();
 	onewire_node_loop(gw);
-
-	unsigned long sleep_time = SLEEP_TIME;
-	if(loop_dht(gw) == false)
-	{
-		sleep_time = SLEEP_TIME/10;
-	}
+	loop_relays(gw);
+	loop_dht(gw);
 
 	//Repeater mode needs not sleeping : process messages instead.
 	unsigned long start_millis = millis();
-	while( ((unsigned long)(millis() - start_millis)) < sleep_time)
+	while( ((unsigned long)(millis() - start_millis)) < SLEEP_TIME)
 	{
 		gw.process();
 	}
@@ -96,17 +80,9 @@ void loop()
 void incomingMessage(const MyMessage &message)
 {
 	// We only expect one type of message from controller. But we better check anyway.
-	if (message.type == V_LIGHT)
+	if (incoming_message_relay(gw, message))
 	{
-		// Change relay state
-		digitalWrite((message.sensor-RELAY_1_ID) + (RELAY_1_PIN), message.getBool() ? RELAY_ON : RELAY_OFF);
-		// Store state in eeprom
-		gw.saveState(message.sensor, message.getBool());
-		// Write some debug info
-		Serial.print("Incoming change for sensor:");
-		Serial.print(message.sensor);
-		Serial.print(", New status: ");
-		Serial.println(message.getBool());
+		Serial.println("Incomming msg was for relay");
 	}
 	else
 	{
