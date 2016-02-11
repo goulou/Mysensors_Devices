943b135418208ea1a74f59f34cd619b256ecce11
goulou <fredericg_99@yahoo.fr>
2016-02-11 19:39:27 +0100

    update HeatingControl node

--- Devices/HeatingControl/main.cpp
+++ Devices/HeatingControl/main.cpp
@@ -4,12 +4,15 @@
 #include <HardwareSerial.h>
 #include <EEPROM.h>
 #include <LiquidCrystal.h>
+#include <EmonLib.h>
 
 #include <printf.hpp>
 
-#define RELAY_1_ID   16  // MySensors node ID of relay 1
-#define RELAY_1_PIN  22  // Arduino Digital I/O pin number for first relay (second on pin+1 etc)
 #define NUMBER_OF_RELAYS 16 // Total number of attached relays
+const uint8_t relay_pins[NUMBER_OF_RELAYS] PROGMEM = {22, 24, 26, 28, 23, 25, 27, 29, 34, 35, 36, 37, 38, 39, 40, 41};
+const uint8_t relay_ids [NUMBER_OF_RELAYS] PROGMEM = {16, 17, 18, 19, 20, 21, 22, 23, 32, 33, 34, 35, 36, 37, 38, 39};
+
+
 #define RELAY_ON 1  // GPIO value to write to turn on attached relay
 #define RELAY_OFF 0 // GPIO value to write to turn off attached relay
 
@@ -27,6 +30,20 @@ const char Str_Time[] PROGMEM = __TIME__;
 
 
 void incomingMessage(const MyMessage &message);
+void present_relays();
+
+uint8_t get_relay_pin_for_id(uint8_t sensor_id)
+{
+	for(int i=0; i<NUMBER_OF_RELAYS; i++)
+	{
+		if(pgm_read_byte(&relay_ids[i]) == sensor_id)
+		{
+			return pgm_read_byte(&relay_pins[i]);
+		}
+	}
+	return 255;
+}
+
 
 void setup()
 {
@@ -52,8 +69,8 @@ void setup()
 //	lcd.print("ROM cleared");
 //	delay(1000);
 	// Initialize library and add callback for incoming messages
-
-	gw.begin(incomingMessage, AUTO, true, AUTO);
+	Serial.println("Initializing Radio");
+	gw.begin(incomingMessage, 0x13, true, AUTO);
 	// Send the sketch version information to the gateway and Controller
 	gw.sendSketchInfo(xstr(PROGRAM_NAME), "1.0");
 
@@ -68,8 +85,8 @@ void setup()
 	// Fetch relay status
 	for (int i=0; i < NUMBER_OF_RELAYS; i++)
 	{
-		int sensor = RELAY_1_ID + i;
-		int pin = RELAY_1_PIN + i;
+		int sensor = pgm_read_byte(&relay_ids[i]);
+		int pin = get_relay_pin_for_id(sensor);
 		Serial.print("Presenting relay ");
 		Serial.println(sensor);
 		// Register all sensors to gw (they will be created as child devices)
@@ -83,11 +100,50 @@ void setup()
 		bool value = gw.loadState(sensor);
 		gw.send(msg.set(value == HIGH ? 1 : 0));
 	}
+
+	gw.present(8, S_POWER);
 }
+
+void present_relays()
+{
+	for (int i=0; i < NUMBER_OF_RELAYS; i++)
+	{
+		int sensor = pgm_read_byte(&relay_ids[i]);
+		int pin = get_relay_pin_for_id(sensor);
+		Serial.print("Presenting relay ");
+		Serial.println(sensor);
+		// Register all sensors to gw (they will be created as child devices)
+		gw.present(sensor, V_LIGHT);
+		bool value = gw.loadState(sensor);
+		MyMessage msg(sensor, V_TRIPPED);
+		gw.send(msg.set(value == HIGH ? 1 : 0));
+	}
+}
+
+void send_current_measurments()
+{
+	EnergyMonitor emon1;
+	emon1.current(A0, 60);
+	double Irms = emon1.calcIrms(1480);
+	MyMessage msg(8, V_WATT);
+	msg.set(Irms*230, 4);
+	gw.send(msg);
+}
+
 void loop()
 {
-	// Alway process incoming messages whenever possible
-	gw.process();
+	unsigned long next_update = millis() + 24L*60L*60L*1000L;
+	while((long)next_update - (long)millis() > 0)
+	{
+		unsigned long next_reading = millis() + 30*1000;
+		while(millis() < next_reading)
+		{
+			// Alway process incoming messages whenever possible
+			gw.process();
+		}
+		send_current_measurments();
+	}
+//	present_relays();
 }
 
 void incomingMessage(const MyMessage &message)
@@ -95,8 +151,9 @@ void incomingMessage(const MyMessage &message)
 	// We only expect one type of message from controller. But we better check anyway.
 	if (message.type == V_LIGHT)
 	{
+		int pin = get_relay_pin_for_id(message.sensor);
 		// Change relay state
-		digitalWrite((message.sensor-RELAY_1_ID) + (RELAY_1_PIN), message.getBool() ? RELAY_ON : RELAY_OFF);
+		digitalWrite(pin, message.getBool() ? RELAY_ON : RELAY_OFF);
 		// Store state in eeprom
 		gw.saveState(message.sensor, message.getBool());
 		// Write some debug info
