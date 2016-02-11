30ee4b3ab0e6a61f8f325bd962f0874a098f8ad3
goulou <fredericg_99@yahoo.fr>
2016-02-11 19:36:10 +0100

    add EEPROM_Clear_Mega2560

---
+++ Devices/EEPROM_Clear_Mega2560/main.cpp
@@ -0,0 +1,29 @@
+// Example sketch showing how to send in OneWire temperature readings
+#include <Arduino.h>
+#include <EEPROM.h>
+
+#include <printf.hpp>
+void setup()
+{
+	Serial.begin(115200);
+	printf_begin();
+
+	Serial.println("Clearing EEPROM");
+	for (int i=0;i<512;i++)
+	{
+		EEPROM.write(i, 0xff);
+	}
+}
+
+
+void loop()
+{
+	while(true)
+	{
+		Serial.println("EEPROM cleared : waiting in endless loop");
+		delay(2000);
+	}
+}
+
+
+
