7cb7daec47bf854deb08568d8d8e4f97d3e7e51e
goulou <fredericg_99@yahoo.fr>
2016-08-18 16:42:02 +0200

    format

--- Common/default_mapping.hpp
+++ Common/default_mapping.hpp
@@ -19,16 +19,21 @@
 
 
 #ifdef BOARD_V1
+
 #define EEPROM_RESET_PIN 4
 #define INPUT_PINS_DEFAULT 3, 6, 8, A1
 #define OUTPUT_PINS_DEFAULT 7, 5, A3, A2
+
 #elif defined(BOARD_V2)
+
 #define INPUT_PINS_DEFAULT 2, 3, 6, 5
 #define OUTPUT_PINS_DEFAULT A1, 8, 7, A2
 #define EEPROM_RESET_PIN A3
 
 #elif defined(BOARD_MEGA)
+
 #define EEPROM_RESET_PIN	13
+
 #endif
 
 #endif //NO_DEFAULT_MAPPING
