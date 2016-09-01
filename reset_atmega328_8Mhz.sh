366e2564dc2a359c11314b22b2ba63b59d8f78e6
goulou <fredericg_99@yahoo.fr>
2016-09-01 22:35:06 +0200

    add confs for project dirs and bootloaders in reset_atmega scripts

--- reset_atmega328_8Mhz.sh
+++ reset_atmega328_8Mhz.sh
@@ -1,5 +1,10 @@
-PROJECT_DIR=$(dirname $0)
-BOOTLOADER=${PROJECT_DIR}/Arduino/hardware/arduino/avr/bootloaders/optiboot/optiboot_atmega328_384_8.hex
+if [ -z "$1" ];
+then
+	PROJECT_DIR=$(dirname $0)
+	BOOTLOADER=${PROJECT_DIR}/Arduino/hardware/arduino/avr/bootloaders/optiboot/optiboot_atmega328_384_8.hex
+else
+	BOOTLOADER=$1
+fi
 
 #set fuses
 avrdude  -c usbasp -p atmega328p -P usb -b 115200 -u -U lfuse:w:0xe2:m -U hfuse:w:0xd9:m -U efuse:w:0xff:m
