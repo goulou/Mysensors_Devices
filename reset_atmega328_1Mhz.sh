a6fba7b2596d3ddcc7f58959728f68a2f2cd829f
goulou <fredericg_99@yahoo.fr>
2016-09-13 18:07:02 +0200

    update reset scripts

--- reset_atmega328_1Mhz.sh
+++ reset_atmega328_1Mhz.sh
@@ -7,7 +7,7 @@ else
 fi
 
 #set fuses
-avrdude  -c usbasp -p atmega328p -P usb -b 115200 -u -U lfuse:w:0x62:m -U hfuse:w:0xd9:m -U efuse:w:0xff:m
+avrdude  -c usbasp -p atmega328p -P usb -b 115200 -u -U lfuse:w:0x62:m -U hfuse:w:0xd9:m -V -U efuse:w:0xff:m || exit -1
 
 
 avrdude  -c usbasp -p atmega328p -P usb -b 38400 -e -U flash:w:${BOOTLOADER} -D -U lock:w:0x2f:m
--- reset_atmega328_8Mhz.sh
+++ reset_atmega328_8Mhz.sh
@@ -7,8 +7,8 @@ else
 fi
 
 #set fuses
-avrdude  -c usbasp -p atmega328p -P usb -b 115200 -u -U lfuse:w:0xe2:m -U hfuse:w:0xd9:m -U efuse:w:0xff:m
+avrdude  -c usbasp -p atmega328p -P usb -b 115200 -u -U lfuse:w:0xe2:m -U hfuse:w:0xd9:m -V -U efuse:w:0xff:m || exit -1
 
 
-avrdude  -c usbasp -p atmega328p -P usb -b 38400 -e -U flash:w:${BOOTLOADER} -D -U lock:w:0x2f:m
+avrdude  -c usbasp -p atmega328p -P usb -b 38400 -e -U flash:w:${BOOTLOADER} -D -U lock:w:0x2f:m || exit -1
 
